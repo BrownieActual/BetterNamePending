ef40b84c7320836b04fb8ad5233c312f17091717
Th3VileOne <alvinatebowo@gmail.com>
2025-09-15 18:23:01 +0700

    chore: remove  unused code

--- components/PageContent.tsx
+++ components/PageContent.tsx
@@ -11,16 +11,3 @@ const PageContent = ({ children }: PropsWithChildren) => {
 };
 
 export default PageContent;
-
-// const Table = React.forwardRef<
-//   HTMLTableElement,
-//   React.HTMLAttributes<HTMLTableElement>
-// >(({ className, ...props }, ref) => (
-//   <div className="relative w-full overflow-auto">
-//     <table
-//       ref={ref}
-//       className={cn("w-full caption-bottom text-sm", className)}
-//       {...props}
-//     />
-//   </div>
-// ));
